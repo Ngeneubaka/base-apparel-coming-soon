84e9a37cfc517c86d117791df6815908600ba893
ubakang <ngeneubaka1@gmail.com>
2023-01-13 10:56:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const submit = document.querySelector(".submit");
+const error = document.querySelector(".error");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const label = document.querySelector("label");
+
+form.addEventListener("submit", validate);
+submit.addEventListener("click", validate);
+
+function validate(e) {
+    e.preventDefault();
+
+    const inputValue = input.value.trim();
+    console.log(inputValue);
+
+    if (!isEmail(inputValue)) {
+        error.style.display = "block";
+    } else {
+        error.style.display = "none";
+    }
+}
+
+function isEmail(input) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)
+}
